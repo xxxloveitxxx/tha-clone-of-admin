3ba9db3f6fc237a064c18155ea5c1b6111dc614b
xxxloveitxxx <socialorbit1@gmail.com>
2025-09-17 22:11:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -473,7 +473,7 @@ def track_click(lead_id, campaign_id):
         }).execute()
         
         # Redirect to the demo page with lead_id as parameter
-        demo_url = "https://closefaster.vercel.app/templates/demo2.html"
+        demo_url = "https://xxxloveitxxx.github.io/tha-clone-of-admin/templates/demo6.html"
         redirect_url = f"{demo_url}?lead_id={lead_id}&campaign_id={campaign_id}"
         if email_queue_id:
             redirect_url += f"&eqid={email_queue_id}"
